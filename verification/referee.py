4c8ce0bd75cfdba83449a95bf504052d5f026252
oduvan <a.lyabah@checkio.org>
2016-06-13 19:41:59 +0300

    js covercode

--- verification/referee.py
+++ verification/referee.py
@@ -12,7 +12,8 @@ api.add_listener(
         tests=TESTS,
         cover_code={
             'python-27': cover_codes.unwrap_args,
-            'python-3': cover_codes.unwrap_args
+            'python-3': cover_codes.unwrap_args,
+            'js-node': cover_codes.js_unwrap_args
         },
         checker=checkers.float_comparison(2)
     ).on_ready)
